220f06ec06cc19caeaf17b720b0c7088c281da95
msalas <msalas@vectoritcgroup.com>
2018-05-04 20:14:15 +0200

    config

--- src/main/java/com/msl/data/arangodb/promo/loader/RepositoryConfig.java
+++ src/main/java/com/msl/data/arangodb/promo/loader/RepositoryConfig.java
@@ -2,10 +2,10 @@ package com.msl.data.arangodb.promo.loader;
 
 public class RepositoryConfig {
 
-	public static final int NUM_EMPRESAS = 5;
-	public static final int NUM_CENTROS = 10;
-	public static final int NUM_FAMILIAS = 100;
-	public static final int NUM_MARCAS = 100;
-	public static final int NUM_PRODUCTOS = 1000;
-	public static final int NUM_PROMOS = 10;
+	public static final int NUM_EMPRESAS = 2;
+	public static final int NUM_CENTROS = 2;
+	public static final int NUM_FAMILIAS = 2;
+	public static final int NUM_MARCAS = 2;
+	public static final int NUM_PRODUCTOS = 2;
+	public static final int NUM_PROMOS = 2;
 }
